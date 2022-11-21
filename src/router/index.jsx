2620512c8121c48533726ed962fd9149a4d82eb8
Guilherme Daniel <gui.dani13@gmail.com>
2022-11-21 08:27:53 -0300

    layout funcionando

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AuthLayout } from "../components/layouts/AuthLayout";
 import { MainLayout } from "../components/layouts/MainLayout";
+import { App } from "../views/App";
 import { Dashboard } from "../views/Dashboard";
 import { Errorpage } from "../views/Errorpage";
 import { ForgotPassword } from "../views/ForgotPassword";
@@ -9,26 +10,31 @@ import { Signup } from "../views/Signup";
 
 export const router = createBrowserRouter([
   {
-    element: <AuthLayout />,
+    path: "/",
+    element: <App />,
     errorElement: <Errorpage />,
     children: [
       {
-        index: true,
-        path: "/",
-        element: <Signin />,
-      },
-      {
-        path: "register",
-        element: <Signup />,
-      },
-      {
-        path: "forgot-password",
-        element: <ForgotPassword />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
+        element: <AuthLayout />,
+        children: [
+          {
+            index: true,
+            element: <Signin />,
+          },
+          {
+            path: "register",
+            element: <Signup />,
+          },
+          {
+            path: "forgot-password",
+            element: <ForgotPassword />,
+          },
+          {
+            path: "signup",
+            element: <Signup />,
+          },
+        ],
+      }
     ],
   },
 ]);
